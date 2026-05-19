a43694be0fe196caeb5bbb752279e2d20b21b97c
lungfachang-hash <lungfa.chang@gmail.com>
2026-05-19 21:18:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,59 +1,59 @@
 import streamlit as st
 import datetime
+
 st.set_page_config(page_title="微型 TimeTree", layout="wide")
+mode = st.radio("選擇群組" , ["學生" , "老師" , "家長會" , "校友會"],horizontal=True)
 
-with st.sidebar:
-    st.write("###  行事曆群組")
-    st.radio("選擇群組", ["工作","家庭", "家庭","朋友"])
+if "mylist" not in st.session_state:
+    st.session_state.mylist = []
+l , r = st.columns(2)
 
-#建立三個不等寬的欄位：左欄放輸入(1)、中欄放看板(2)、右欄放設定(1.2)
+def get_color(group):
+    if group == "學生":
+        return "#E3F2FD"
+    elif group == "老師":
+        return "#E6F5D9"
+    elif group == "家長會":
+        return "#FFF3E0"
+    else:
+        return "#F3E5F5"
+with l:
+    t1=st.text_input("行程主旨")
+    t3=st.date_input("日期選擇" , datetime.date.today())
+    t4=st.time_input("時間選擇")
+    n1= st.number_input(
+     "行程開始前幾分鐘提醒？",
+     min_value=0, max_value=60,
+     value=15
+    )
+    if st.button("新增行程"):
+        st.session_state.mylist.append({
+            "group": mode,
+            "title": t1,
+            "date": str(t3),
+            "time": str(t4),
+            "remind": n1
+        })
+with r:
 
-col_left, col_center, col_right = st.columns([1, 2, 1.2], gap="large")
-
-with col_left: 
-    with st.container(border=True):     
-        st.write("行程描述") 
-
-    title = st.text_input("行程主旨",placeholder="請填寫會議名稱...")
-
-today = st.date_input("選擇日期",  datetime.date.today())
-
-meeting_time = st.time_input("選擇時間")
- text = st.time_input("時間：09:00")
-    
-my_color = st.color_picker(
- "挑選辨識顏色",
- "#1A73E8"
-)
-
-   
-    @st.dialog("新增完成")
-    def showAdd():        
-        st.write(f"新增行程{txt} 成功")
-    showAdd() 
-    st.button("按鈕放左邊")
-    
-with col_center: 
-    st.write("###  看板區")
-    st.info("主要行程訊息放中間")
-    
-with col_right: 
-    st.write("###  行程檢視")
-    tab1 , tab2 , =st.tabs(["本月行程" , "已封存的行程"])
-
-with tab1:    
-    with st.container(border=True):
-        st.write("本月行程")
-with tab2:    
-    with st.container(border=True):
-        st.write("已封存的行程")
-
-st.write("上面是大標題")
-st.divider()
-st.write("下面是內容區塊")        
-st.button("按鈕 A")
-st.write("")   #塞入一哥空白間距
-st.button("按鈕 B")
+    with st.container(border = True):
+        for item in st.session_state.mylist:
+            color = get_color(item["group"])
+       
+            st.markdown(f"""
+            <div style="
+                background-color:{color};
+                padding:15px;
+                border-radius:12px;
+                margin-bottom:10px;
+            ">
+                <b>{item["group"]}</b><br>
+                📌 {item["title"]}<br>
+                📅 {item["date"]}<br>
+                ⏰ {item["time"]}<br>
+                🔔 提前 {item["remind"]} 分鐘
+            </div>
+            """, unsafe_allow_html=True)
         
 
 
